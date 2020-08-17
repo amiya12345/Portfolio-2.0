9aed6433000c128f884360022f17e772f2a007b6
Amiya Ranjan sethi <amiyaranjansethi0551@gmail.com>
2020-08-18 03:07:17 +0530

    js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,5 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 0);
 })
+document.getElementById("name").innerHTML = "Amiya Ranjan";
+document.getElementById("skills").innerHTML=" UI/UX Designer | UI Developer";
