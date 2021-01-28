ac19e264d5baa8067e4f677f30992d3d07bb69de
Amiya Ranjan sethi <amiyaranjansethi0551@gmail.com>
2021-01-28 19:51:05 +0530

    js link update

--- js/index.js
+++ js/index.js
@@ -6,3 +6,8 @@ document.getElementById("name").innerHTML = "Amiya Ranjan";
 document.getElementById("skills").innerHTML=" Digital Product Designer";
 document.getElementById("other_works").innerHTML="OTHERS CREATIVE WORKS";
 
+function jump(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+
