d33c09d60957954fe0d087c5f35b8348189cc1a9
Amiya Ranjan sethi <amiyaranjansethi0551@gmail.com>
2021-02-02 01:07:23 +0530

    amiya

--- js/index.js
+++ js/index.js
@@ -2,9 +2,7 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 0);
 })
-document.getElementById("name").innerHTML = "Amiya Ranjan";
-document.getElementById("skills").innerHTML=" Digital Product Designer";
-document.getElementById("other_works").innerHTML="OTHERS CREATIVE WORKS";
+
 
 function jump(url) {
     var win = window.open(url, '_blank');
